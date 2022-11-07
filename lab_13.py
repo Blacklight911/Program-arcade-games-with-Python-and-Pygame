8de9052a0ad20d560d36a1af005029ea20c7cd8f
Blacklight <dmitreykyrchin@gmail.com>
2022-11-07 14:25:34 +0300

    Continued lab_13.py added png files to lab_13.py

--- lab_13.py
+++ lab_13.py
@@ -1,45 +1,82 @@
 """
-Use sprites to collect blocks.
-
-Sample Python/Pygame Programs
-Simpson College Computer Science
-http://programarcadegames.com/
-http://simpson.edu/computer-science/
-
+This is lab_13 haaaaalp
 """
-import random
 import pygame
-
+import random
 
 # Define some colors
-BLACK = (0, 0, 0)
+BLACK = ( 0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+
+
+class Player(pygame.sprite.Sprite):
+	"""
+	The class is the player-controlled sprite
+	"""
+	# -- Methods
+	def __init__(self, x, y):
+		"""Constructor function"""
+		# Call the parent's constructor
+		super().__init__()
+
+		# Set height, width
+		self.image = pygame.Surface([15, 15])
+		# Load image
+		self.image = pygame.image.load('ufo.png').convert()
+		self.image.set_colorkey(BLACK)
+		# Make our top-left corner the passed-in location.
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+
+		# -- Attributes
+		# Set speed vector
+		self.change_x = 0
+		self.change_y = 0
+
+	def changespeed(self, x, y):
+		""" Change the speed of the player"""
+
+		self.change_x += x
+		self.change_y += y
+
+	def update(self):
+		""" Find a new position for the player"""
+
+		self.rect.x += self.change_x
+		self.rect.y += self.change_y
 
 
 class Block(pygame.sprite.Sprite):
-    """
-    This class represents the ball.
-    It derives from the "Sprite" class in Pygame.
-    """
+	"""
+	This class represents the ball.
+	It derives from the "Sprite" class in Pygame.
+	"""
 
-    def __init__(self, color, width, height):
-        """ Constructor. Pass in the color of the block,
-        and its size. """
+	def __init__(self, filename, width, height):
+		""" Constructor. Pass in the color of the block, and its size. """
 
-        # Call the parent class (Sprite) constructor
-        super().__init__()
+		# Call the parent class (Sprite) constructor
+		super().__init__()
 
-        # Create an image of the block, and fill it with a color.
-        # This could also be an image loaded from the disk.
-        self.image = pygame.Surface([width, height])
-        self.image.fill(color)
+		# Create an image of the block, and fill it with a color.
+		# This could also be an image loaded from the disk.
+		self.image = pygame.image.load(filename).convert()
 
-        # Fetch the rectangle object that has the dimensions of the image
-        # image.
-        # Update the position of this object by setting the values
-        # of rect.x and rect.y
-        self.rect = self.image.get_rect()
+		# Set background color to be transparent. Adjust to WHITE if your
+		# background is WHITE.
+		self.image.set_colorkey(BLACK)
+
+		# Create an image of the block, and fill it with a color.
+		# This could also be an image loaded from the disk.
+		self.image = pygame.Surface([width, height])
+
+		# Fetch the rectangle object that has the dimensions of the image
+		# Update the position of this object by setting the values of rect.x and rect.y
+		self.rect = self.image.get_rect()
 
 
 # Initialize Pygame
@@ -52,26 +89,40 @@ screen = pygame.display.set_mode([screen_width, screen_height])
 
 # This is a list of 'sprites.' Each block in the program is
 # added to this list. The list is managed by a class called 'Group.'
-block_list = pygame.sprite.Group()
-
+good_block_list = pygame.sprite.Group()
+bad_block_list = pygame.sprite.Group()
 # This is a list of every sprite.
 # All blocks and the player block as well.
+
 all_sprites_list = pygame.sprite.Group()
 
 for i in range(50):
-    # This represents a block
-    block = Block(BLACK, 20, 15)
+	# This represents a block
+	block = Block('star.png', 20, 15)
 
-    # Set a random location for the block
-    block.rect.x = random.randrange(screen_width)
-    block.rect.y = random.randrange(screen_height)
+	# Set a random location for the block
+	block.rect.x = random.randrange(screen_width)
 
-    # Add the block to the list of objects
-    block_list.add(block)
-    all_sprites_list.add(block)
+	block.rect.y = random.randrange(screen_height)
 
-# Create a RED player block
-player = Block(RED, 20, 15)
+	# Add the block to the list of objects
+	good_block_list.add(block)
+	all_sprites_list.add(block)
+
+for i in range(50):
+	# This represents a block
+	block = Block('alien.png', 20, 15)
+
+	# Set a random location for the block
+	block.rect.x = random.randrange(screen_width)
+	block.rect.y = random.randrange(screen_height)
+
+	# Add the block to the list of objects
+	bad_block_list.add(block)
+	all_sprites_list.add(block)
+
+# Create a Blue player block
+player = Player(20, 15)
 all_sprites_list.add(player)
 
 # Loop until the user clicks the close button.
@@ -83,39 +134,61 @@ clock = pygame.time.Clock()
 score = 0
 
 # -------- Main Program Loop -----------
+
 while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			done = True
 
-    # Clear the screen
-    screen.fill(WHITE)
+# Set the speed based on the key pressed
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_a:
+				player.changespeed(-3, 0)
+			elif event.key == pygame.K_d:
+				player.changespeed(3, 0)
+			elif event.key == pygame.K_w:
+				player.changespeed(0, -3)
+			elif event.key == pygame.K_s:
+				player.changespeed(0, 3)
 
-    # Get the current mouse position. This returns the position
-    # as a list of two numbers.
-    pos = pygame.mouse.get_pos()
 
-    # Fetch the x and y out of the list,
-    # just like we'd fetch letters out of a string.
-    # Set the player object to the mouse location
-    player.rect.x = pos[0]
-    player.rect.y = pos[1]
+# Reset speed when key goes up
+			elif event.type == pygame.KEYUP:
+				if event.key == pygame.K_a:
+					player.changespeed(3, 0)
+				elif event.key == pygame.K_d:
+					player.changespeed(-3, 0)
+				elif event.key == pygame.K_w:
+					player.changespeed(0, 3)
+				elif event.key == pygame.K_s:
+					player.changespeed(0, -3)
 
-    # See if the player block has collided with anything.
-    blocks_hit_list = pygame.sprite.spritecollide(player, block_list, True)
+	# Clear the screen
+	screen.fill(WHITE)
 
-    # Check the list of collisions.
-    for block in blocks_hit_list:
-        score += 1
-        print(score)
+	# Fetch the x and y out of the list,
+	# just like we'd fetch letters out of a string.
 
-    # Draw all the spites
-    all_sprites_list.draw(screen)
+	all_sprites_list.update()
+	# See if the player block has collided 	with anything.
 
-    # Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
+	blocks_hit_list = pygame.sprite.spritecollide(player, good_block_list, True)
+	blocks_hit_list_red = pygame.sprite.spritecollide(player, bad_block_list, True)
+	# Check the list of collisions.
+	for block in blocks_hit_list:
+		score += 1
+		print(score)
 
-    # Limit to 60 frames per second
-    clock.tick(60)
+	for block in blocks_hit_list_red:
+		score -= 1
+		print(score)
+	# Draw all the spites
+	all_sprites_list.draw(screen)
+
+	# Go ahead and update the screen with 	what we've drawn.
+	pygame.display.flip()
+
+	# Limit to 60 frames per second
+	clock.tick(60)
 
 pygame.quit()
